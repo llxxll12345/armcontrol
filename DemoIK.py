977f2d14f53d3f5fb5a9769004d9ed58df305e6e
llxxll12345 <charlieliu@liulixings-MacBook-Pro.local>
2019-10-31 15:20:37 -0400

    mod

--- DemoIK.py
+++ DemoIK.py
@@ -5,6 +5,7 @@
 #  Simple demo of meArm library to walk through some points defined in Cartesian coordinates
 
 import meArm
+import time
 
 def main():
     arm = meArm.meArm()
@@ -17,22 +18,37 @@ def main():
         arm.openGripper()
 
         arm.gotoPoint(0, 10, 20)
+        time.sleep(0.5)
         arm.gotoPoint(0, 20, 50)
+        time.sleep(0.5)
         arm.gotoPoint(0, 50, 70)
+        time.sleep(0.5)
         arm.gotoPoint(0, 20, 100)
+        time.sleep(0.5)
         arm.gotoPoint(1, 1, 1)
+        time.sleep(0.5)
         arm.gotoPoint(50, 50, 100)
+        time.sleep(0.5)
         arm.gotoPoint(20, 30, 50)
+        time.sleep(0.5)
         arm.gotoPoint(10, 10, 20)
-
-        arm.gotoPoint(0, 10, 20)
-        arm.gotoPoint(0, 20, 50)
-        arm.gotoPoint(0, 50, 70)
-        arm.gotoPoint(0, 20, 100)
-        arm.gotoPoint(1, 1, 1)
-        arm.gotoPoint(50, 50, 100)
-        arm.gotoPoint(20, 30, 50)
-        arm.gotoPoint(10, 10, 20)
+        time.sleep(0.5)
+        arm.gotoPoint(0, -10, 20)
+        time.sleep(0.5)
+        arm.gotoPoint(0, -20, 50)
+        time.sleep(0.5)
+        arm.gotoPoint(0, -50, 70)
+        time.sleep(0.5)
+        arm.gotoPoint(0, -20, 100)
+        time.sleep(0.5)
+        arm.gotoPoint(1, -1, 1)
+        time.sleep(0.5)
+        arm.gotoPoint(50, -50, 100)
+        time.sleep(0.5)
+        arm.gotoPoint(20, -30, 50)
+        time.sleep(0.5)
+        arm.gotoPoint(10, -10, 20)
+        time.sleep(0.5)
 
 
 if __name__ == '__main__':
